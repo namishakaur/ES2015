d90a9b6f764d450534a4f805f6874b51ee980220
Namisha Kaur <namishakaur@Namishas-MacBook-Pro.local>
2023-06-24 16:24:12 -0500

    finished destructuring exercise

---
+++ destructuring.js
@@ -0,0 +1,33 @@
+//1.
+//8
+//1846
+
+//2.
+//{yearNeptuneDiscovered: 1846, yearsMarsDiscovered: 1659}
+
+//3.
+//Your first name is Alejandro and you like purple
+//Your first name is Melissa and you like green
+// Your name is undefined and you like green
+
+//4.
+//Maya
+//Marissa
+//Chi
+
+//5.
+//Raindrops on roses
+//whiskers on kittens
+//{"Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"}
+
+//6.
+//20, 10, 30
+
+//7.
+// const {a, b} = obj.numbers
+
+//8
+//[arr[0],arr[1]]= [arr[1],arr[0]]
+
+//9
+// const raceResults = ([first, second, third, ...rest]=> ({first, second, third, rest})
